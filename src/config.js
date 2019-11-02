a68805646bac02c0874c4637d74e7c09454a4588
slark1995 <610158299@qq.com>
2019-11-01 20:46:29 -0600

    finished

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyA5t4KHs3xsyJNYeQnadtRUKFrWH_XA3Mg",
+    authDomain: "tj-blog-1eff5.firebaseapp.com",
+    databaseURL: "https://tj-blog-1eff5.firebaseio.com",
+  }
+  
+  export default config;
